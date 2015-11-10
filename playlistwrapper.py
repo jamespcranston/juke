45670b2edca367d0791af3d61a2bc52a33f3d9bd
Conner Smith <csmith@DN0a223644.SUNet>
2015-11-10 15:03:57 -0800

    replaced trackwrapper module with defaultdict maintained inside playlistwrapper

--- playlistwrapper.py
+++ playlistwrapper.py
@@ -1,11 +1,25 @@
 from spotify import Playlist
-import TrackWrapper
 
 class PlaylistWrapper(Playlist):
 	def __init__(self):
 		super().__init__()
+		self.ranking_map = defaultdict(int)
 
-	# call this whenever 
+
+
+	# call this whenever any upvote/downvote is pressed or song is queued to preserve order
 	def sort_tracks(self):
-		self.tracks = sorted(self.tracks, key = lambda track: track.net_vote)
+		self.tracks = sorted(self.tracks, key = lambda track: ranking_map[track])
+
+	def add_tracks(self, tracks):
+		# if a track is being queued for the first time, net_vote == 1
+		# if track already in queue, increment net_vote
+		for track in tracks:
+			ranking_map[track] += 1
+		self.sort_tracks()
+
+	
+
+
+
 
--- trackwrapper.py
+++
@@ -1,7 +0,0 @@
-from spotify import Track
-
-class TrackWrapper(Track):
-	def __init__(self):
-		super().__init__()
-		self.net_vote = 0
-		
