b6b4b4a3976ba65171a6cc7f9c114c093b03b10f
Conner Smith <csmith@rescomp-15-314755.stanford.edu>
2015-11-17 12:10:50 -0800

    added basic funtionality

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
 import spotify
+import searcher
+
 
 global session
+global playlist_container
+global curr_playlist
+global audio_sink
+
+def show_playlists():
+	for index, playlist in enumerate(playlist_container):
+		playlist = playlist.load()
+		print('Index: {} Name: {} Number of Tracks: {}'.format(index, playlist.name, len(playlist.tracks)))
+	index = int(input('*** choose index of playlist to join: '))
+	curr_playlist = playlist_container[index]
+	print('Successfully joined {}!'.format(curr_playlist.name))
+
+def show_songs_on_playlist():
+	for index, track  in enumerate(curr_playlist.tracks):
+		print('Index: {} Track: {}  Artist: {} Popularity: {}\n'.format(index, track.name, track.artists[0].load().name,
+		 	track.popularity))
+
+def play_song(track):
+	session.player.load(track)
+	session.player.play()
+
+def add_song():
+	track_name = input('*** track to search for: ')
+	search = searcher.Searcher(session)
+	results = search.find_track_list(track_name, session)
+	for index, entry in enumerate(results):
+		print('Index: {} Track: {}  Artist: {} Popularity: {}\n'.format(index, entry['track'].name, entry['artist'],
+		 entry['popularity']))
+	index = int(input('*** choose index of song to add: '))
+	curr_playlist.add_tracks(results[index]['track'])
+	print('{} successfully added to {}!'.format(results[index]['track'].name, curr_playlist.name))
+
+def handle_playlist_action():
+	while True:
+		choice = input('*** (a)dd a song, (p)lay a song, (v)iew all playlists, (b)ack \n*** ').lower()[0]
+		if choice == 'a':
+			add_song()
+		elif choice == 'p':
+			show_songs_on_playlist()
+			index = int(input('*** choose index of song to play: '))
+			play_song(curr_playlist.tracks[index])
+			stop = input("*** hit 's' to pause song playing \n").lower()[0]
+			if stop == 's':
+				session.player.pause()
+		elif choice == 'v':
+			show_playlists()
+		elif choice == 'q':
+			break
 
 def create_playlist():
 	name = input('playlist name: ')
-	track_name = input('track to search for: ')
-	search = session.search(track_name)
-	search.load()
-	print(search.tracks)
+	playlist_container.add_new_playlist(name, 0)
+	global curr_playlist
+	curr_playlist = playlist_container[0]
 
+	handle_playlist_action()
+	
 def join_playlist(index):
 	pass
 
@@ -16,26 +67,32 @@ def show_nearby_playlists():
 	pass
 
 def handle_main_menu():
-	action = input('(v)iew nearby playlists, (c)reate a playlist, or (q)uit? ').lower()[0]
+	action = input('*** (c)reate a playlist or (q)uit? ').lower()[0]
+	'''
 	if action == 'v':
 		show_nearby_playlists()
-		playlist_index = input('enter the index of playlist you want to join: ')
+		playlist_index = input('*** enter the index of playlist you want to join: ')
 		join_playlist(playlist_index)
-	elif action == 'c':
+	'''
+	if action == 'c':
 		create_playlist()
 
 def login():
-	user = input('spotify username: ')
-	password = input('spotify password: ')
+	user = input('*** spotify username: ')
+	password = input('*** spotify password: ')
 	session.login(user, password)
 	while session.connection.state != spotify.ConnectionState.LOGGED_IN:
 		session.process_events()
 	print('Login successful!')
 
 if __name__ == '__main__':
-	print('Welcome to Juke!')
+	print('*** Welcome to Juke! ***')
 	session = spotify.Session()
 	login()
+	playlist_container = session.playlist_container.load()
+	for index, playlist in enumerate(playlist_container):
+		playlist_container.remove_playlist(index)
+	audio_sink = spotify.PortAudioSink(session)
 	handle_main_menu()
 
 
--- searcher.py
+++ searcher.py
@@ -2,10 +2,28 @@
 import spotify
 
 class Searcher():
+	def __init__(self, session):
+		self.session = session
+
 	# given a track name, this method should return a list containing a dictionary for 
 	# top 10 search results. Format: 
 	# [{'track': ~ ,artist': ~, album': ~ , 'duration': ~ , 'popularity': ~ }, etc.]
 	# note that 'track' should map to an actual track object that will later be used by the audio player 
 	# to load/play the track
-	def find_track_list(track_name):
-		pass
+	def find_track_list(self, track_name, session):
+		tracks = session.search(track_name).load().tracks
+		if len(tracks) is 0:
+			return
+
+		list_to_return = []
+		for index in range(len(tracks)):
+			if index > 9:
+				break
+			d = {}
+			track = tracks[index].load()
+			d['track'] = track
+			d['artist'] = track.artists[0].load().name
+			d['album'] = track.album.load().name
+			d['popularity'] = track.popularity
+			list_to_return.append(d)
+		return list_to_return
